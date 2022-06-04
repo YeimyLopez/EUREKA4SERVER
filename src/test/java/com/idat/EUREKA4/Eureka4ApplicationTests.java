079ddf32f3dd804d22b3e32f19544b30a45c8833
Piero Nicolas Quispe Pozo <pieronicolas333@gmail.com>
2022-06-04 00:01:43 -0500

    EVAL 4 - Piero nicolas quispe pozo -EUREKA

---
+++ src/test/java/com/idat/EUREKA4/Eureka4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.EUREKA4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eureka4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
